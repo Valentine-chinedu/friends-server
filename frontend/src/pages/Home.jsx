bbd62bf3c5f10e489ff0cf4309d39b5575f350aa
Valentine <okosival@gmail.com>
2022-09-19 18:58:53 +0100

    fix: fixed getNextPage error

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,43 @@
-import CreatePost from '../shared components/CreatePost';
-import Online from '../shared components/Online';
-import Profile from '../shared components/ProfileCard';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import useFetch from '../hooks/useFetch';
+import { setAllPosts } from '../redux/slices/postSlice';
+import { fetchPosts } from '../services/postServices';
+import CreatePost from '../sharedcomponents/CreatePost';
+import InfinityScroll from '../sharedcomponents/InfinityScroll';
+import Online from '../sharedcomponents/Online';
+import Posts from '../sharedcomponents/Posts';
+import ProfileCard from '../sharedcomponents/ProfileCard';
 
 const Home = () => {
+	const {
+		post: {
+			allPosts: { posts, page },
+		},
+		user: { id },
+	} = useSelector((state) => state);
+
+	const customFetch = useFetch();
+	const dispatch = useDispatch();
+
+	const getNextPage = async () => {
+		const data = await customFetch(fetchPosts, { page: page + 1 });
+		dispatch(setAllPosts({ posts: posts.concat(data.posts), page: data.page }));
+		return data.posts.length;
+	};
+
 	return (
-		<section>
-			<div>
-				<Profile />
+		<section className='home'>
+			<div className='home__left'>
+				<ProfileCard id={id} isOwnProfile />
 			</div>
-			<main>
-				<CreatePost />
-				<Post />
-			</main>
-			<aside>
+			<InfinityScroll getNextPage={getNextPage}>
+				<main className='home__center'>
+					<CreatePost />
+					<Posts posts={posts} />
+				</main>
+			</InfinityScroll>
+			<aside className='home__right gradient-border'>
 				<Online />
 			</aside>
 		</section>
--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import notfound from '../../assets/NotFound.png';
 
 const NotFound = () => {
-	return <div>NotFound</div>;
+	const navigate = useNavigate();
+	return (
+		<div className='notfound'>
+			<img src={notfound} alt='not-found' />
+			<h1>Not Found</h1>
+			<button onClick={() => navigate('/')}>Go Home</button>
+		</div>
+	);
 };
 
 export default NotFound;
