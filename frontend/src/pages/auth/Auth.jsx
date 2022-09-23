325994bc8d347118207cfe7fa0e943bac52da683
Valentine <okosival@gmail.com>
2022-09-23 13:10:44 +0100

    fix: removed routing

--- frontend/src/pages/auth/Auth.jsx
+++ frontend/src/pages/auth/Auth.jsx
@@ -1,14 +1,17 @@
-import { render } from 'react-dom';
-import { Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import Login from '../../sharedcomponents/Login';
+import Register from '../../sharedcomponents/Register';
+
+import './auth.css';
 
 const Auth = () => {
+	const [isRegistering, setIsRegistering] = useState(false);
+
 	return (
-		<Routes>
-			<Route path='auth' element={<Auth />} />
-			<Route index element={<Login />} />
-			<Route path='register' element={<Register />} />
-			<Route path='login' element={<Login />} />
-		</Routes>
+		<section className={isRegistering ? 'auth signup' : 'auth'}>
+			<Login setIsRegistering={setIsRegistering} />
+			<Register setIsRegistering={setIsRegistering} />
+		</section>
 	);
 };
 
