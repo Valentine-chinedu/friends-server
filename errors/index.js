bea2f73a8ac97f3851ae4c647bb2e9f8d5db6325
Valentine <okosival@gmail.com>
2022-10-02 22:03:02 +0100

    setup vercel config

---
+++ errors/index.js
@@ -0,0 +1,11 @@
+const NotFoundError = require('./not-found');
+const BadRequestError = require('./bad-request');
+const CustomError = require('./custom-error');
+const AuthenticationError = require('./authentication-error');
+
+module.exports = {
+	NotFoundError,
+	BadRequestError,
+	CustomError,
+	AuthenticationError,
+};
