bac1aa72681d479fa30dbb74696072317310fbfe
Valentine-chinedu <okosival@gmail.com>
2023-04-10 21:30:08 +0100

    modified client url

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const server = require('http').createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server, {
 	cors: {
-		origin: 'https://friends-client.vercel.app/',
+		origin: 'https://friends-client.vercel.app',
 	},
 });
 
